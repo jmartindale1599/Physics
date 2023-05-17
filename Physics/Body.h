fdee196ce29632e0a1a8af056bec77d2f6575ae2
jmartindale1599 <98477132+jmartindale1599@users.noreply.github.com>
2023-05-17 10:54:49 -0600

    Started implementation for Joints

--- Physics/Body.h
+++ Physics/Body.h
@@ -12,8 +12,6 @@ public:
 
 	Body(Shape* shape, const glm::vec2& position, const glm::vec2& velocity = { 0, 0 }, float mass = 1, Type type = Type::Dynamic) : shape{ shape }, position { position }, velocity{ velocity }, mass{ mass }, type{ type } {
 	
-		if (type == Static) mass = 0;
-
 		invMass = (mass == 0) ? 0 : 1 / mass; 
 	
 	};
